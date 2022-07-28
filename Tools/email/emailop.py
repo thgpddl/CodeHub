07ea88e437e749070f69647f09bfc95e92fc8259
thgpddl <48787805+thgpddl@users.noreply.github.com>
2022-07-28 13:16:30 +0800

    Update emailop.py

--- Tools/email/emailop.py
+++ Tools/email/emailop.py
@@ -31,5 +31,5 @@ class EmailOP:
         self.smtp.sendmail(from_addr="fairy12138@126.com", to_addrs="353254167@qq.com", msg=message.as_string())
 
 
-emailop = EmailOP(host="smtp.126.com", port=25, user="fairy12138@126.com", password="UUSCLRAZVPSYGVNA")
+emailop = EmailOP(host="smtp.126.com", port=25, user="fairy135158@126.com", password="UUSCLRAZVPSYGVNA")
 emailop.send(From="hyj", To="qq", Subject="test", Context="python test")
